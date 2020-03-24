c2e2e62021b565c5b36674a62e10ef06678306dd
Carlo Cassanelli <carlo.cassanelli95@gmail.com>
2020-03-24 14:56:28 +0100

    aggiunti numeri andamento nazionale

---
+++ js/andamento.js
@@ -0,0 +1,14 @@
+$(function() {
+
+    var url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json";
+
+    $.getJSON(url, function (data) {
+        var numeri = data[0];
+
+        $("#totale-casi").html(numeri["totale_casi"]);
+        $("#attualmente-positivi").html(numeri["totale_attualmente_positivi"]);
+        $("#deceduti").html(numeri["deceduti"]);
+        $("#guariti").html(numeri["dimessi_guariti"]);
+        $("#data").html(numeri["data"]);
+    });
+});
